fbebf566e40623d69f7fc8cc02dccb8249a7d516
Shubham Shinde <shubhamshinde.3151@gmail.com>
2020-12-09 16:29:03 +0530

    Fonts added.

--- src/App.js
+++ src/App.js
@@ -33,11 +33,21 @@ const App = () => {
   };
 
   return (
-    <div className="main">
-      <Loading />
+    <div className="main-wrapper">
+      <p className="f1">The quick brown fox jumps over the lazy dog</p>
+      <p className="f2">The quick brown fox jumps over the lazy dog</p>
+      <p className="f3">The quick brown fox jumps over the lazy dog</p>
+      <p className="f4">The quick brown fox jumps over the lazy dog</p>
+      <br />
+      <p className="f5">The quick brown fox jumps over the lazy dog</p>
+      <p className="f6">The quick brown fox jumps over the lazy dog</p>
+      <p className="f7">The quick brown fox jumps over the lazy dog</p>
+      <p className="f8">The quick brown fox jumps over the lazy dog</p>
+      <p className="f9">The quick brown fox jumps over the lazy dog</p>
+      <p className="f10">The quick brown fox jumps over the lazy dog</p>
       <Snackbar open={snackbar.open} message={snackbar.message} severity={snackbar.severity} onClose={handleClose} />
       <Backdrop open={backdrop} />
-      <Button onClick={handleClick}>Open</Button>
+      <Button onClick={handleClick} variant="contained" color="primary">Open</Button>
     </div>
   );
 };
--- src/components/common/Backdrop.js
+++ src/components/common/Backdrop.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     color: '#fff',
+    zIndex: 1000,
   },
 }));
 
