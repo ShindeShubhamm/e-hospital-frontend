9fb536877c5f2b6729b6d1c276f19ecc64593bb6
Shubham Shinde <shubhamshinde.3151@gmail.com>
2020-12-21 22:33:02 +0530

    Navbar and side links added.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,19 +3,39 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Container from '@material-ui/core/Container';
 import Toolbar from '@material-ui/core/Toolbar';
-import { MdAccountCircle } from 'react-icons/md';
+import { MdMenu } from 'react-icons/md';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = (props) => {
+  const { navLinks, handleDrawer } = props;
+
   return (
     <div className="header">
       <AppBar>
         <Container>
           <Toolbar>
             <div className="h-contents">
-              <div className="h-logo">
-                <h2>eHospital</h2>
+              <div className="h-left-contents">
+                <button type="button" className="h-menu-button" onClick={() => handleDrawer(true)}>
+                  <MdMenu className="h-menu-icon" />
+                </button>
+                <Link to="/" className="h-logo-container">
+                  <img src="/images/rx_online.svg" alt="Rx.Online" className="h-logo" />
+                </Link>
               </div>
-              <MdAccountCircle size={28} />
+              <div className="h-right-contents">
+                {navLinks.map((link) => (
+                  <NavLink
+                    exact
+                    to={link.route}
+                    key={link.route}
+                    className="h-link"
+                    activeClassName="h-link-active"
+                  >
+                    {link.name}
+                  </NavLink>
+                ))}
+              </div>
             </div>
           </Toolbar>
         </Container>
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import { NavLink } from 'react-router-dom';
 
 import Footer from './Footer';
 import Header from './Header';
 
+const navLinks = [
+  { name: 'Home', route: '/' },
+  { name: 'Login', route: '/login' },
+  { name: 'Signup', route: '/signup' },
+];
+
 const Layout = (props) => {
+  const [drawer, setDrawer] = useState(false);
+
+  const handleDrawer = (state) => {
+    setDrawer(state);
+  };
+
   return (
     <div className="layout">
-      <Header />
+      <Header navLinks={navLinks} handleDrawer={handleDrawer} />
       <div className="l-children">
         {props.children}
       </div>
       <Footer />
-    </div>
+      <SwipeableDrawer
+        open={drawer}
+        onOpen={() => handleDrawer(true)}
+        onClose={() => handleDrawer(false)}
+      >
+        <div className="l-side-links">
+          {navLinks.map((link) => (
+            <NavLink
+              exact
+              to={link.route}
+              key={link.route}
+              onClick={() => handleDrawer(false)}
+              className="l-sidelink"
+              activeClassName="l-sidelink-active"
+            >
+              {link.name}
+            </NavLink>
+          ))}
+        </div>
+      </SwipeableDrawer>
+    </div >
   );
 };
 
